fcfe134aa37125eb6ff7f65a2c77309e67608091
Mahmud sajib <mahmudsajib082@gmail.com>
2023-10-12 13:25:37 +0600

    router setup

---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    <div>
+    
+    this is Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae perspiciatis et magnam, consequatur rem soluta quibusdam, quidem facilis officia, minima aperiam commodi voluptatum. Nulla aliquam nam, quasi molestiae rerum nesciunt?
+    
+    </div>
+  )
+}
